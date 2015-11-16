fa1d4b90e8306c34a5fa024d14dc900c35ae5ec0
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-16 17:07:36 +0000

    add required modules

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require("express-session");
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
 var bodyParser = require('body-parser');
+var multer = require("multer");
+var flash = require("connect-flash");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
