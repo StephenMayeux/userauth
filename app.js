1d3097ec24af89755d36c2e8cba1f3ea019a6b0f
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-16 17:23:43 +0000

    add middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require("express-session");
+var expressValidator = require("express-validator");
 var passport = require("passport");
 var localStrategy = require("passport-local").Strategy;
 var bodyParser = require('body-parser');
@@ -23,13 +24,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// handle file uploads
+app.use(multer({dest: './uploads'}));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle express sessions
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport Session middleware always comes after express session middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
