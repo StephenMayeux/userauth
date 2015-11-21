1b50844e480bff20018c7013303be04ee9344143
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-21 02:37:04 +0000

    add login functionality

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user'); // models are uppercase and singular
 
 /* GET users listing. */
@@ -81,6 +83,42 @@ router.post('/register', function(req, res, next) {
   // success message
   req.flash('success', 'You are now registered');
   res.location('/');
+  res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.getUserByUsername(username, function(err, user) {
+      if (err) throw err;
+      if (!user) {
+        console.log('Unknown user');
+        return done(null, false, {message: 'unknown user'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          console.log('that password does not match homie');
+          return done(null, false, {message: 'Invalid password'});
+        }
+      });
+    });
+}));
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/users/login', failureFlash: 'Invalid Username or Password'}), function(req, res) {
+  console.log('Authentication successful');
+  req.flash('success', 'You are logged in');
   res.redirect('/');
 });
 
