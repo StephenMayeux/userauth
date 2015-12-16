358d2913c339c26566e6b3352aefa91a38573de0
Stephen Mayeux <stephenmayeux@gmail.com>
2015-12-16 15:26:53 +0000

    bug fixes

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var bcrypt = require("bcrypt");
-mongoose.connect('mongodb://localhost/nodeauth');
+mongoose.connect(process.env.MONGOLAB_URI);
 var db = mongoose.connection;
 
 // user schema
